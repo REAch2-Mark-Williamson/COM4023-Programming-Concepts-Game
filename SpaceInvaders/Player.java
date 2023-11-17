035c68264d435f527b0674b2f5ce698a194c89d2
REAch2-Mark-Williamson <147151953+REAch2-Mark-Williamson@users.noreply.github.com>
2023-11-17 15:12:45 +0000

    Bullet spawn

--- SpaceInvaders/Player.java
+++ SpaceInvaders/Player.java
@@ -12,13 +12,27 @@ public class Player extends Actor
      * Act - do whatever the Player wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    private int Player_Speed = 5;
+    
     public void act()
     {
         if (Greenfoot.isKeyDown("a")){
-            move(-5);
+            move(- Player_Speed);
         }
         if (Greenfoot.isKeyDown("d")){
-            move(5);
+            move(Player_Speed);
         }
+        if (Greenfoot.isKeyDown("space")){
+            Player_Bullet_Spawn();
+        }
+    }
+    
+    //Spawn player bullet
+    public void Player_Bullet_Spawn (){
+        World Current_World=getWorld();
+        int Player_X_Pos = getX();
+        int Player_Y_Pos = getY();
+        Current_World.addObject(new Player_Bullet(), Player_X_Pos, Player_Y_Pos);
     }
 }
