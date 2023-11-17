01f4f74b10a8703f19ab443c990cf54ce1caabff
WillMarsdenSTFC <will.marsden@stfc.ac.uk>
2023-11-17 15:22:42 +0000

    Commit

--- SpaceInvaders/MyWorld.java
+++ SpaceInvaders/MyWorld.java
@@ -16,6 +16,9 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        GreenfootImage background = getBackground();
+        background.setColor(Color.BLACK);
+        background.fillRect(0,0,getWidth(),getHeight());
     }
 }
