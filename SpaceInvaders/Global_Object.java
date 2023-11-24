77671db1194ee9200d963f5a05202379164c397a
REAch2-Mark-Williamson <147151953+REAch2-Mark-Williamson@users.noreply.github.com>
2023-11-24 14:02:24 +0000

    HP and Global_Object

---
+++ SpaceInvaders/Global_Object.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Global_Object here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Global_Object extends Actor
+{
+    //Destroy actors
+    public void Destroy_Object() {
+        getWorld().removeObject(this);
+    }
+}
